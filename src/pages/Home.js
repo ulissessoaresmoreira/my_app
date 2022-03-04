f0806ffe07498f3b284eed81500710412b82f720
Ulisses Soares <ulissessm@gmail.com>
2022-03-04 21:47:36 +0000

    estrutura do projeto react material ui

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Header from '../partials/Header'
+
+const Home = () => {
+
+    return (
+        
+            <>
+            </>
+            
+        
+    )
+}
+
+
+export default Home
