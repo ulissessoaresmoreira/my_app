1b4f4c6143b208e7cdb8185bc19f4f3dd66026b2
Ulisses Soares <ulissessm@gmail.com>
2022-03-07 11:50:14 +0000

    Ukraine

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -7,12 +7,34 @@ import {
     Button,
     IconButton,    
 } from '@mui/material'
+
 import MenuIcon from '@mui/icons-material/Menu'
+import styled from 'styled-components'
+//import { makeStyles } from '@mui/material' NÃO FUNCIONA
+
+
+const MyApp = styled.div`
+    color: #0057B8;
+    background-color: #FFD500;
+    width: 90px;
+    font-weight: 900;   
+    margin-left: 1px; 
+    padding: 0px;    
+    display: flex;    
+    justify-content: center;    
+`
+
+const Menu = styled.div`
+    color:#FFD500;
+    font-weight: 900;
+`
 
 
 const Header = () => {
+    
 
     return (
+        
         <AppBar position="static">
             <Toolbar>
             <IconButton
@@ -22,14 +44,15 @@ const Header = () => {
                 aria-label="menu"
                 sx={{ mr: 2 }}
             >
-            <MenuIcon />
+            <Menu><MenuIcon /></Menu>
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                News
-            </Typography>
-            <Button color="inherit">Login</Button>
+                <MyApp>News</MyApp>
+            </Typography>                                  
+            <Button color="inherit"><MyApp>Login</MyApp></Button>
             </Toolbar>
         </AppBar>
+        
     
     )
 }
